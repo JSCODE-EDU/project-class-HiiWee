2f9cb7df36088617984d0aa9a272440fc1b994c8
HiiWee <wpdlzhf159@gmail.com>
2023-05-11 08:43:02 +0900

    style: 인덴트 정리

--- anonymous-board/src/test/java/com/example/anonymousboard/post/repository/PostRepositoryTest.java
+++ anonymous-board/src/test/java/com/example/anonymousboard/post/repository/PostRepositoryTest.java
@@ -3,9 +3,7 @@ package com.example.anonymousboard.post.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.anonymousboard.config.JpaConfig;
-import com.example.anonymousboard.post.domain.Content;
 import com.example.anonymousboard.post.domain.Post;
-import com.example.anonymousboard.post.domain.Title;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
