f0674514d79992c6292da15ea49f5742a7dd94bc
HiiWee <wpdlzhf159@gmail.com>
2023-05-27 02:55:55 +0900

    refactor: api docs 테스트 유틸 패키지 분리

---
+++ anonymous-board/src/test/java/com/example/anonymousboard/util/apidocs/DocumentFormatGenerator.java
@@ -0,0 +1,13 @@
+package com.example.anonymousboard.util.apidocs;
+
+
+import static org.springframework.restdocs.snippet.Attributes.key;
+
+import org.springframework.restdocs.snippet.Attributes;
+
+public class DocumentFormatGenerator {
+
+    public static Attributes.Attribute getConstraints(final String key, final String value) {
+        return key(key).value(value);
+    }
+}
